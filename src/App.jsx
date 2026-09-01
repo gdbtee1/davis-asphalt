4313fa10fda139fcf8e552b791c77f212409d8ee
Techuvo <techuvo.dev@gmail.com>
2026-09-01 15:01:56 -0400

    Update Davis Asphalt homepage and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Services from './pages/Services'
@@ -7,14 +7,14 @@ import About from './pages/About'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
